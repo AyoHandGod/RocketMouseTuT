d668669b5fb3a8baa80ee2c7273bfe833c76e68a
Dayo <admin@nerdroco.com>
2018-08-31 22:08:44 -0700

    MouseController
    Added MouseController Script

---
+++ Assets/RocketMouse/Scripts/MouseController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    // Public Fields
+    public float jetpackForce = 75f;  // force applied to mouse when jet pack is on
+
+    // Private Fields
+    private Rigidbody2D rb;         // rigidbody 
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>(); // get the rigidbody component of the object script is applied to
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // use fixedUpdate to check for input constantly
+    private void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("Fire1"); // creates a boolean to check for input
+        if (jetpackActive)                             // checks if boolean above true
+        {
+            rb.AddForce(new Vector2(0, jetpackForce));  // Apply jetpack force to rigidbody causing movement. vector to specifies x, and y movements applied. 
+        }
+    }
+}
