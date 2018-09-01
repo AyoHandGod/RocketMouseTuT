ee215d0e6880415aa497c02c5001fc712ac144e1
Dayo <admin@nerdroco.com>
2018-09-01 01:31:34 -0700

    Ground Check
    Added Ground Check. Updated MouseController to support GroundCheck

--- Assets/RocketMouse/Scripts/MouseController.cs
+++ Assets/RocketMouse/Scripts/MouseController.cs
@@ -8,12 +8,18 @@ public class MouseController : MonoBehaviour {
     public float jetpackForce = 75f;  // force applied to mouse when jet pack is on
     public float forwardMovementSpeed = 3.0f;  // forward movement force
 
+    public Transform groundCheckTransform;     // transform of our groundCheck child object
+    public LayerMask groundCheckLayerMask;     // Layer Mask of our groundCheck child object
+    
+
     // Private Fields
     private Rigidbody2D rb;         // rigidbody 
-
+    private bool isGrounded;       // is groundCheck touching the floor?
+    private Animator animator;     // grab our animator
 
 	// Use this for initialization
 	void Start () {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>(); // get the rigidbody component of the object script is applied to
 	}
 	
@@ -33,5 +39,14 @@ public class MouseController : MonoBehaviour {
         Vector2 newVelocity = rb.velocity;  // set a vector2 to the value of our objects rigidbody velocity
         newVelocity.x = forwardMovementSpeed;  // update the x value of newVelocity to equal our forwardMovementSpeed
         rb.velocity = newVelocity;  // Update objects rigidbody velocity using new newVelocity values
+        UpdateGroundedStatus();    // calls updatedGroundedStatus check function
     }
+
+    void UpdateGroundedStatus()
+    {
+        // if the groundCheck object touches something on the specified layer, is grounded becomes true
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        animator.SetBool("isGrounded", isGrounded); // uses isGrounded to switch animation condition isGrounded
+    }
+
 }
