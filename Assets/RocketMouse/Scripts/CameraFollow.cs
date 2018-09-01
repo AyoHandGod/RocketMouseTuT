0407b6124a3a8ad735903c3d4353abce7cc9cd24
Dayo <admin@nerdroco.com>
2018-08-31 22:53:18 -0700

    Movement and Camera Follow
    Added Character movement to MouseController script. Created CameraFollow script to follow object in scene.

---
+++ Assets/RocketMouse/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;  // Game object that will be our target
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float targetObjectX = target.transform.position.x; // grabs the x transform position of target
+        Vector3 newCameraPosition = transform.position; // create a Vector3 holding current transform position of camera
+        newCameraPosition.x = targetObjectX;    // updates x value of our newCameraPosition vector3 to that of our target object
+        transform.position = newCameraPosition;  // updates camera transform position using newCameraPosition vector3
+	}
+}
--- Assets/RocketMouse/Scripts/MouseController.cs
+++ Assets/RocketMouse/Scripts/MouseController.cs
@@ -6,6 +6,7 @@ public class MouseController : MonoBehaviour {
 
     // Public Fields
     public float jetpackForce = 75f;  // force applied to mouse when jet pack is on
+    public float forwardMovementSpeed = 3.0f;  // forward movement force
 
     // Private Fields
     private Rigidbody2D rb;         // rigidbody 
@@ -29,5 +30,8 @@ public class MouseController : MonoBehaviour {
         {
             rb.AddForce(new Vector2(0, jetpackForce));  // Apply jetpack force to rigidbody causing movement. vector to specifies x, and y movements applied. 
         }
+        Vector2 newVelocity = rb.velocity;  // set a vector2 to the value of our objects rigidbody velocity
+        newVelocity.x = forwardMovementSpeed;  // update the x value of newVelocity to equal our forwardMovementSpeed
+        rb.velocity = newVelocity;  // Update objects rigidbody velocity using new newVelocity values
     }
 }
