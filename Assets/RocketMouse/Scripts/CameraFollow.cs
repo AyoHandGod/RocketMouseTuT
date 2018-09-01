95649d1b7ea88e8429ece490114b66305c0f1e6e
Dayo <admin@nerdroco.com>
2018-08-31 23:02:05 -0700

    Prefabs
    Stored Objects as Prefabs. Updated CameraFollow script so that it maintains distance from followed object.

--- Assets/RocketMouse/Scripts/CameraFollow.cs
+++ Assets/RocketMouse/Scripts/CameraFollow.cs
@@ -6,16 +6,20 @@ public class CameraFollow : MonoBehaviour {
 
     public GameObject target;  // Game object that will be our target
 
+    private float distanceToTarget; // float to hold initial distance from camera to target
+
 	// Use this for initialization
 	void Start () {
-		
+        distanceToTarget = transform.position.x - target.transform.position.x;  // calculates distance from target at start
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float targetObjectX = target.transform.position.x; // grabs the x transform position of target
         Vector3 newCameraPosition = transform.position; // create a Vector3 holding current transform position of camera
-        newCameraPosition.x = targetObjectX;    // updates x value of our newCameraPosition vector3 to that of our target object
+        newCameraPosition.x = targetObjectX + distanceToTarget;    // updates x value of our newCameraPosition vector3 to that of our target object
+                                                                   // and maintains initial distance from target. (Remove distanceToTarget to have camera
+                                                                   // follow on top of object. This code will maintain a distance.
         transform.position = newCameraPosition;  // updates camera transform position using newCameraPosition vector3
 	}
 }
